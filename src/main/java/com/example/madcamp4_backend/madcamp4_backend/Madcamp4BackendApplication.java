93cd9544bbe18c9a8e506d6228f8330a9b8ae403
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-19 12:36:14 +0900

    initial commit

---
+++ src/main/java/com/example/madcamp4_backend/madcamp4_backend/Madcamp4BackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.madcamp4_backend.madcamp4_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+public class Madcamp4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Madcamp4BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/madcamp4_backend/madcamp4_backend/Madcamp4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madcamp4_backend.madcamp4_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Madcamp4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
