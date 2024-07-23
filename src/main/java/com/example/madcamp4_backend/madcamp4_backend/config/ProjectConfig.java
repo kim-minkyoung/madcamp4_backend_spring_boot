3b0b182ee5e3e066752bd5107fcb13d6818635e8
seoyeong <carsk0607@gmail.com>
2024-07-24 01:30:53 +0900

    test

--- src/main/java/com/example/madcamp4_backend/madcamp4_backend/config/ProjectConfig.java
+++ src/main/java/com/example/madcamp4_backend/madcamp4_backend/config/ProjectConfig.java
@@ -6,11 +6,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import java.io.IOException;
 
 @Configuration
 public class ProjectConfig {
@@ -31,22 +31,22 @@ public class ProjectConfig {
         };
     }
 
-    @Bean
-    public OncePerRequestFilter corsFilter() {
-        return new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException {
-                response.addHeader("Access-Control-Allow-Origin", "*");
-                response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-                response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-                response.addHeader("Access-Control-Expose-Headers", "Authorization");
-
-                if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-                    response.setStatus(HttpServletResponse.SC_OK);
-                } else {
-                    filterChain.doFilter(request, response);
-                }
-            }
-        };
-    }
+//    @Bean
+//    public OncePerRequestFilter corsFilter() {
+//        return new OncePerRequestFilter() {
+//            @Override
+//            protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException {
+//                response.addHeader("Access-Control-Allow-Origin", "*");
+//                response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//                response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+//                response.addHeader("Access-Control-Expose-Headers", "Authorization");
+//
+//                if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+//                    response.setStatus(HttpServletResponse.SC_OK);
+//                } else {
+//                    filterChain.doFilter(request, response);
+//                }
+//            }
+//        };
+//    }
 }
